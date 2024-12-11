4695ccf2cf1be8a5990eb96cf6f56a2f7858979a
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2024-12-11 14:37:29 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -16,3 +16,23 @@ export default defineStackbitConfig({
     ],
     "postInstallCommand": "npm i --no-save @stackbit/types @stackbit/cms-contentful"
 })
+import { defineStackbitConfig } from "@stackbit/types";
+import { ContentfulContentSource } from "@stackbit/cms-contentful";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0",
+  ssgName: "nextjs",
+  nodeVersion: "16",
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID!,
+      environment: process.env.CONTENTFUL_ENVIRONMENT!,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
+      useWebhookForContentUpdates: true
+    })
+  ],
+  models: {
+    page: { type: "page", urlPath: "/{slug}" }
+  }
+});
