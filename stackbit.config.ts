0ea04e9c4b77043608e3fc2cc0121e6649be2f14
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2024-12-20 09:32:41 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,28 +1,26 @@
+iimport { defineStackbitConfig } from '@stackbit/types';
 import { ContentfulContentSource } from '@stackbit/cms-contentful';
 
-import { defineStackbitConfig } from '@stackbit/types';
-
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "nextjs",
-    "contentSources": [
+    stackbitVersion: "~0.6.0",
+    nodeVersion: "18",
+    ssgName: "nextjs",
+    contentSources: [
         new ContentfulContentSource({
             spaceId: process.env.CONTENTFUL_SPACE_ID!,
             environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
-            accesssToken:process.env.CONTENFUL_ACCESS_TOKEN,
             previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
-            managementToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
-            useWebhookForContentUpdates: true, 
-           
-    ],
-            
+            accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
+            useWebhookForContentUpdates: true,
         }),
-})
-import { defineStackbitConfig } from "@stackbit/types";
-import { ContentfulContentSource } from "@stackbit/cms-contentful";
-export const addconfig = {
-       contentSources: [ Contentful ]
-              modelExtensions: [{ name: 'Untitled', type: 'Invoice', urlPath: '/{slug}' }],
-    }
+    ],
+    modelExtensions: [
+        {
+            name: "Untitled",
+            type: "Invoice",
+            urlPath: "/{slug}",
+        },
+    ],
+    postInstallCommand: "npm i --no-save @stackbit/types @stackbit/cms-contentful",
 });
+
