0209d8628371ebf0ab2ed3553c30022eb2eb4345
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2024-12-27 16:38:21 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -7,7 +7,6 @@ export default defineStackbitConfig({
     ssgName: "nextjs",
     contentSources: [
         new ContentfulContentSource({
-            name:'contentful',
             spaceId: process.env.CONTENTFUL_SPACE_ID!,
             environment: process.env.CONTENTFUL_ENVIRONMENT!,
             previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
