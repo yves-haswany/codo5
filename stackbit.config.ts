181ff5bc43e4935004ffdcd203bfe64b6783b8a7
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2024-12-30 09:00:26 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,7 +2,7 @@ import { defineStackbitConfig } from '@stackbit/types';
 import { ContentfulContentSource } from '@stackbit/cms-contentful';
 
 export default defineStackbitConfig({
-    stackbitVersion: "~0.6.0",
+    stackbitVersion: "0.3.127",
     nodeVersion: "18",
     ssgName: "nextjs",
     contentSources: [
