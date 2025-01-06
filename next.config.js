b2067bd8f8406a0953fc755799a48bf1f5ea3f7f
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-01-06 13:05:43 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
-};
-
-module.exports : {
-   
   experimental: {
     serverActions: true,
   },
 };
+
+module.exports = nextConfig;
+
