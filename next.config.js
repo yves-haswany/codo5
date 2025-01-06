b0e7f8c8bd9f759bbbab72ad4af52a5015e27db9
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-01-06 13:19:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: true, // This must be an object, not a boolean.
   },
 };
 
