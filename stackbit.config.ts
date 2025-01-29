72d5590250f1fc58803bc78b8e3538462b85dede
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-01-29 11:25:26 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -24,5 +24,33 @@ export default defineStackbitConfig({
             urlPath: "/{slug}"
         }
     ],
+     siteMap: ({ documents, models }) => {
+    // 1. Filter all page models which were defined in modelExtensions
+    const pageModels = models.filter((m) => m.type === "page")
+
+    return documents
+      // 2. Filter all documents which are of a page model
+      .filter((d) => pageModels.some(m => m.name === d.modelName))
+      // 3. Map each document to a SiteMapEntry
+      .map((document) => {
+        // Map the model name to its corresponding URL
+        const urlModel = (() => {
+            switch (document.modelName) {
+                case 'Page':
+                    return 'otherPage';
+                default:
+                    return null;
+            }
+        })();
+
+        return {
+          stableId: document.id,
+          urlPath: `/${urlModel}/${document.id}`,
+          document,
+          isHomePage: document.modelName === 'homePage',
+        };
+      })
+      .filter(Boolean) as SiteMapEntry[];
+  }
     postInstallCommand: "npm i --no-save @stackbit/types @stackbit/cms-contentful"
 });
