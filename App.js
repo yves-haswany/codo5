82bec8b8ebb30ca2d498af4d2e0562bb5f290f48
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-01-29 17:52:34 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ApiDataComponent from './components/ApiDataComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Codo5</h1>
+      <ApiDataComponent />  {/* Display API data here */}
+    </div>
+  );
+};
