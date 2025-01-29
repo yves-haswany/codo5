a091143dfb5a19225dbd3a7977a3d91ae5a552b7
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-01-29 12:30:14 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -12,17 +12,45 @@ export default defineStackbitConfig({
             spaceId: process.env.CONTENTFUL_SPACE_ID!,
             environment: process.env.CONTENTFUL_ENVIRONMENT! || 'master',
             previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
-            accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!!,
+            accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
             
             useWebhookForContentUpdates: true
         }) // Corrected: Closing the configuration object properly
     ],
     modelExtensions: [
         {
-            name: "Untitled",
-            type: "Invoice",
-            urlPath: "https://app.contentful.com/spaces/j86lyi7eedea/visual_modeler/content_types/invoice"
+            name: "homePage",
+            type: "page",
+            urlPath: "/{slug}"
         }
     ],
+     siteMap: ({ documents, models }) => {
+    // 1. Filter all page models which were defined in modelExtensions
+    const pageModels = models.filter((m) => m.type === "page")
+
+    return documents
+      // 2. Filter all documents which are of a page model
+      .filter((d) => pageModels.some(m => m.name === d.modelName))
+      // 3. Map each document to a SiteMapEntry
+      .map((document) => {
+        // Map the model name to its corresponding URL
+        const urlModel = (() => {
+            switch (document.modelName) {
+                case 'Page':
+                    return 'otherPage';
+                default:
+                    return null;
+            }
+        })();
+
+        return {
+          stableId: document.id,
+          urlPath: `/${urlModel}/${document.id}`,
+          document,
+          isHomePage: document.modelName === 'homePage',
+        };
+      })
+      .filter(Boolean) as SiteMapEntry[];
+  },
     postInstallCommand: "npm i --no-save @stackbit/types @stackbit/cms-contentful"
 });
