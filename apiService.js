2301a9aaa4763204e9fbc106b1517eb3d54cffaf
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-01-31 12:46:51 +0200

    Update apiService.js

--- apiService.js
+++ apiService.js
@@ -1,6 +1,7 @@
+// apiService.js
 export const fetchApiData = async () => {
   try {
-    const response = await fetch('https://api.develop.rve.ca/docs#/', {
+    const response = await fetch('https://api.develop.rve.ca/v1/modules/c667ff46-9730-425e-ad48-1e950691b3f9/measuring-points/71ef9476-3855-4a3f-8fc5-333cfbf9e898/reads?start=2024-10-16&end=2024-11-25&limit=50&offset=0'', {
       method: 'GET',  // or POST if needed
       headers: {
         'Authorization': process.env.API_KEY, // If required
