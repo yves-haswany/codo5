a7eef66b87f90e5c26100015c56a21eb980d5eff
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-02-03 11:38:11 +0200

    Update apiService.js

--- apiService.js
+++ apiService.js
@@ -1,4 +1,8 @@
 // apiService.js
+const startDate = '2024-10-16';
+const endDate = '2024-11-25';
+const limit = 50;
+const offset = 0;
 export const fetchApiData = async (startDate, endDate, limit, offset) => {
   try {
     const url = new URL('https://api.develop.rve.ca/v1/modules/c667ff46-9730-425e-ad48-1e950691b3f9/measuring-points/71ef9476-3855-4a3f-8fc5-333cfbf9e898/reads');
