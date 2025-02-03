bdd2753173999b7a09080e9efe119263de65a774
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-02-03 11:33:50 +0200

    Update apiService.js

--- apiService.js
+++ apiService.js
@@ -1,10 +1,16 @@
 // apiService.js
-export const fetchApiData = async () => {
+export const fetchApiData = async (startDate, endDate, limit, offset) => {
   try {
-    const response = await fetch('https://api.develop.rve.ca/v1/modules/c667ff46-9730-425e-ad48-1e950691b3f9/measuring-points/71ef9476-3855-4a3f-8fc5-333cfbf9e898/reads?start=2024-10-16&end=2024-11-25&limit=50&offset=0'', {
+    const url = new URL('https://api.develop.rve.ca/v1/modules/c667ff46-9730-425e-ad48-1e950691b3f9/measuring-points/71ef9476-3855-4a3f-8fc5-333cfbf9e898/reads');
+    url.searchParams.append('start', startDate);
+    url.searchParams.append('end', endDate);
+    url.searchParams.append('limit', limit);
+    url.searchParams.append('offset', offset);
+
+    const response = await fetch(url.toString(), {
       method: 'GET',  // or POST if needed
       headers: {
-        'Authorization': Bearer ${process.env.API_KEY}, // If required
+        'Authorization': `Bearer ${process.env.API_KEY}`, // If required
       },
     });
 
