d4ffa8b29d08980c5c30aeeea4b83a6447ed43fb
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-02-10 14:10:27 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -33,7 +33,7 @@ export default defineStackbitConfig({
                         case 'homePage':
                             return 'Home';
                         case 'otherPage':
-                            return 'page';
+                            return 'invoice';
                         default:
                             return null;
                     }
