f4bcffffa380f44d3f26ca027e3acb965cef1027
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-02-12 13:23:10 +0200

    Update ComposablePage.jsx

--- components/ComposablePage.jsx
+++ components/ComposablePage.jsx
@@ -1,6 +1,6 @@
 export const ComposablePage = (Home) => {
   return (
-    <div data-sb-object-id={props.id}>
+    <div data-sb-object-id={Home.id}>
       <h1 data-sb-field-path="title">Home</h1>
     </div>
   );
