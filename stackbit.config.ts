901b98aaddd397659ad1bc99d4a4755e3235e269
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-02-13 13:02:49 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -48,10 +48,10 @@ export default defineStackbitConfig({
             })
             .filter(Boolean) as SiteMapEntry[];
     },
-    /* assetsConfig: {
+     assetsConfig: {
         uploadDir: "assets/uploads",
         publicPath: "/uploads",
         supportedExtensions: [".jpg", ".jpeg", ".png", ".gif", ".svg"]
-    },*/
+    },
     postInstallCommand: "npm i --no-save @stackbit/types @stackbit/cms-contentful"
 });
