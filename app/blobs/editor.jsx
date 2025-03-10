d55b2fd01383f1a5fe74a3be5564420e7fb475e8
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-03-10 10:50:00 +0200

    Update editor.jsx

--- app/blobs/editor.jsx
+++ app/blobs/editor.jsx
@@ -5,14 +5,23 @@ import { StoredBlobsList } from './list';
 import { NewShape } from './new-shape';
 
 export function ShapeEditor(props) {
-    // Allow new shape editor to signal that a mutation has occured, triggering the list
-    // of stored blobs to be reloaded (you can also use form actions)
     const [lastMutationTime, setLastMutationTime] = useState(0);
+    const [editableContent, setEditableContent] = useState('');
+
+    const handleContentChange = (e) => {
+        setEditableContent(e.target.value);
+    };
 
     return (
         <div className="flex w-full flex-col md:flex-row md:items-start gap-8">
             <div className="md:w-2/5">
                 <NewShape setLastMutationTime={setLastMutationTime} />
+                <input 
+                    type="text" 
+                    value={editableContent} 
+                    onChange={handleContentChange} 
+                    placeholder="Edit content here" 
+                />
             </div>
             <div className='w-full'>
                 <StoredBlobsList lastMutationTime={lastMutationTime} />
