c8f26fd75a0e72cecc186024aa4b98bb8db0e1cd
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2025-03-11 16:59:24 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import ApiDataComponent from './components/ApiDataComponent';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Codo5</h1>
-      <ApiDataComponent />  {/* Display API data here */}
-    </div>
-  );
+const App = ({ content }) => {
+    if (!content) {
+        return <div>Loading...</div>;
+    }
+
+    const { title, description, featuredImage, sections, footer } = content.fields;
+
+    return (
+        <div style={{ backgroundImage: `url(${featuredImage.fields.file.url})` }}>
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <div>
+                {sections && sections.map((section, index) => (
+                    <div key={index}>
+                        <h2>{section.title}</h2>
+                        <p>{section.content}</p>
+                    </div>
+                ))}
+            </div>
+            <footer>
+                <p>{footer.content}</p>
+            </footer>
+        </div>
+    );
 };
+
+export default App;
